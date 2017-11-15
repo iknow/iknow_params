a5fcb077b39c4b8ddd7a0d40f6462edbab76a592
Chris Andreae <chris@bibo.com.ph>
2017-11-15 16:46:59 +0900

    HashParser: be indifferent to symbols
    HashParsers pretend to be parsing Rails controller params, so for the
    interface to correspond the hash should really be accessed
    with_indifferent_access.

--- lib/iknow_params/parser/hash_parser.rb
+++ lib/iknow_params/parser/hash_parser.rb
@@ -1,12 +1,14 @@
- # Simple wrapper to use IknowParams::Parser to extract and verify content from
- # an arbitrary hash
+require "active_support/core_ext/hash/indifferent_access"
+
+# Simple wrapper to use IknowParams::Parser to extract and verify content from
+# an arbitrary hash
 class IknowParams::Parser::HashParser
   include IknowParams::Parser
 
   attr_reader :params
 
   def initialize(view_hash)
-    @params = view_hash
+    @params = ActiveSupport::HashWithIndifferentAccess.new(view_hash)
   end
 
   def parse(&block)
